d29c57dc00673eb1ff7b2dff82efab1dc2ddc8da
Hemant Sahu <shahhemant378@gmai.com>
2025-05-09 09:30:49 +0530

    not thing much added

--- feature.java
+++ feature.java
@@ -2,6 +2,6 @@ package GITDEMO.project2;
 
 public class feature {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world");     
     }
 }
